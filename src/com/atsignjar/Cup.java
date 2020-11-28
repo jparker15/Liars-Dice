9f11f0a3ba871907206d2699513076a01566f640
Jeremy <jeremyeparker@yahoo.com>
2020-11-27 23:26:34 -0500

    add cup class

---
+++ src/com/atsignjar/Cup.java
@@ -0,0 +1,41 @@
+package com.atsignjar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Cup {
+
+        private ArrayList<Die> dice = new ArrayList<>();
+
+        public Cup (int size){
+            for (int count = 0; count < size; count++){
+                dice.add(new Die());
+            }
+        }
+
+        public void roll(Random rand){
+            for (var die: dice) {
+                die.roll(rand);
+            }
+        }
+        public void roll (Random rand, int choice){
+            dice.get(choice).roll(rand);
+        }
+
+        public void roll (Random rand, List<Integer> choices){
+            for (int choice: choices) {
+                dice.get(choice).roll(rand);
+//            roll(rand,choice);
+            }
+        }
+
+        @Override
+        public String toString(){
+            String output = "";
+            for (Die die:dice) {
+                output += die.getValue() + "";
+            }
+            return output.trim();
+        }
+}
